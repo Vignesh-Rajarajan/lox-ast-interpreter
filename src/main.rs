183b807ccd49412a46c7da72970f569adcc68e07
Vignesh Rajarajan <vicky209180@gmail.com>
2024-08-21 14:48:08 +0530

    inital commit with scaffolding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use std::env::args;
+mod error;
+mod scanner;
+mod token;
+mod token_type;
+use scanner::Scanner;
+use std::io::{self, BufRead};
+use token::Token;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: lox-ast [script]");
+        std::process::exit(64);
+    } else if args.len() == 1 {
+        run_file(&args[0]);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(path: &str) -> io::Result<()> {
+    let buf = std::fs::read_to_string(path)?;
+    match run(buf) {
+        Ok(_) => (),
+        Err(err) => {
+            err.report("".to_string());
+            std::process::exit(65);
+        }
+    }
+
+    Ok(())
+}
+fn run_prompt() {
+    let stdin = io::stdin();
+    print!(">");
+    for line in stdin.lock().lines() {
+        if let Ok(line) = line {
+            if line.is_empty() {
+                break;
+            }
+            match run(line) {
+                Ok(_) => (),
+                Err(err) => err.report("".to_string()),
+            }
+        }
+    }
+}
+
+fn run(source: String) -> Result<(), error::LoxError> {
+    let mut scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokens()?;
+    for token in tokens {
+        println!("{:?}", token);
+    }
+    Ok(())
 }
---
+++ src/token.rs
@@ -0,0 +1,62 @@
+use crate::token_type::TokenType;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum Object {
+    String(String),
+    Number(f64),
+    Nil,
+    True,
+    False,
+}
+
+impl Display for Object {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Object::String(s) => write!(f, "{}", s),
+            Object::Number(n) => write!(f, "{}", n),
+            Object::Nil => write!(f, "nil"),
+            Object::True => write!(f, "true"),
+            Object::False => write!(f, "false"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Token {
+    ttype: TokenType,
+    lexeme: String,
+    literal: Option<Object>,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(ttype: TokenType, lexeme: String, literal: Option<Object>, line: usize) -> Self {
+        Token {
+            ttype,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+
+    pub fn eof(line: usize) -> Self {
+        Token::new(TokenType::EOF, "".to_string(), None, line)
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {} {}",
+            self.ttype,
+            self.lexeme,
+            if let Some(literal) = &self.literal {
+                literal.to_string()
+            } else {
+                "nil".to_string()
+            }
+        )
+    }
+}
